dc093584db996cca5c3f415d30b856d9a29f205b
saitej <chinka.saiteja@gmail.com>
2023-05-03 15:49:22 +0530

    src pull

---
+++ catalogue.sh
@@ -0,0 +1,33 @@
+echo -e "\e[36m>>>>>> INSTALLING NODEJS <<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+echo -e "\e[36m>>>>>> ADDING APPLICATION USER <<<<<<\e[0m"
+useradd roboshop
+echo -e "\e[36m>>>>>> CREATING APPLICATION DIRECTORY <<<<<<\e[0m"
+mkdir /app
+echo -e "\e[36m>>>>>> DOWNLOADING APPLICATION CONTENT <<<<<<\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+echo -e "\e[36m>>>>>> REDIRECTING TO /APP DIRECTORY <<<<<<\e[0m"
+cd /app
+echo -e "\e[36m>>>>>> EXTRACTING APPLICATION CONTENT <<<<<<\e[0m"
+unzip /tmp/catalogue.zip
+cd /app
+echo -e "\e[36m>>>>>> DOWNLOADING NODEJS DEPENDENCIES <<<<<<\e[0m"
+npm install 
+echo -e "\e[36m>>>>>> COPYING catalogue service file <<<<<<\e[0m"
+
+cp catalogue.service /etc/systemd/systsem/catalogue.service
+echo -e "\e[36m>>>>>> daemon-reload <<<<<<\e[0m"
+
+systemctl daemon-reload
+echo -e "\e[36m>>>>>> enabling the catalogue <<<<<<\e[0m"
+
+systemctl enable catalogue
+systemctl restart catalogue
+echo -e "\e[36m>>>>>> INSTALLING MANGODB <<<<<<\e[0m"
+
+cp mongo.repo /etc/yum.repo.d/mongo.repo
+yum install mongodb-org-shell-y
+echo -e "\e[36m>>>>>> LOADING SCHEMA <<<<<<\e[0m"
+
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
---
+++ mongodb.sh
@@ -0,0 +1,6 @@
+cp mongo.repo /etc/yum.repo.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod 
+systemctl start mongod 
+sed -i -e "s|127.0.0.1|0.0.0.0|" /etc/mongo.conf
+systemctl restart mongod
