228342af28fe571b843cf92f302a77696ce35e49
saitej <chinka.saiteja@gmail.com>
2023-05-03 18:32:05 +0530

    commit-1

--- frontend.sh
+++ frontend.sh
@@ -9,7 +9,8 @@ rm -rf /usr/share/nginx/html/*
 echo -e "\e[36m>>>>>> Download Application content <<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 echo -e "\e[36m>>>>>> cd to nginx html<<<<<<\e[0m"
-cd /usr/share/nginx/html
+#cd /usr/share/nginx/html
+cd /usr/share/nginx/html 
 echo -e "\e[36m>>>>>> Extract Application conten <<<<<<\e[0m"
 unzip /tmp/frontend.zip
 echo -e "\e[36m>>>>>> copy roboshop.conf <<<<<<\e[0m"
