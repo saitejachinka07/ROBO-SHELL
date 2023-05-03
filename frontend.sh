19f8ce18a27a819d3c4b8b9a6ebdf1f8d54ae91f
saitej <chinka.saiteja@gmail.com>
2023-05-03 18:22:32 +0530

    /usr/share/nginx/html/*

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ systemctl enable nginx
 echo -e "\e[36m>>>>>> Start nginx <<<<<<\e[0m"
 systemctl start nginx
 echo -e "\e[36m>>>>>> Delete initial content in  nginx <<<<<<\e[0m"
-rm -rf /usr/share/nginx/html*
+rm -rf /usr/share/nginx/html/*
 echo -e "\e[36m>>>>>> Download Application content <<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 echo -e "\e[36m>>>>>> cd to nginx html<<<<<<\e[0m"
