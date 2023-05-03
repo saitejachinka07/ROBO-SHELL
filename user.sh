e5448c1b7add9a7c13c951c6487486034172592a
saitej <chinka.saiteja@gmail.com>
2023-05-03 16:21:34 +0530

    catalogue and user

--- user.sh
+++ user.sh
@@ -1,4 +1,4 @@
-echo -e "\e[36m>>>>>> INSTALLING NODEJS <<<<<<\e[0m"
+echo -e "\e[36m>>>>>>> Installing nodejs <<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 echo -e "\e[36m>>>>>> ADDING APPLICATION USER <<<<<<\e[0m"
@@ -6,23 +6,23 @@ useradd roboshop
 echo -e "\e[36m>>>>>> CREATING APPLICATION DIRECTORY <<<<<<\e[0m"
 mkdir /app
 echo -e "\e[36m>>>>>> DOWNLOADING APPLICATION CONTENT <<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
 echo -e "\e[36m>>>>>> REDIRECTING TO /APP DIRECTORY <<<<<<\e[0m"
 cd /app
 echo -e "\e[36m>>>>>> EXTRACTING APPLICATION CONTENT <<<<<<\e[0m"
-unzip /tmp/catalogue.zip
+unzip /tmp/user.zip
 cd /app
 echo -e "\e[36m>>>>>> DOWNLOADING NODEJS DEPENDENCIES <<<<<<\e[0m"
 npm install 
-echo -e "\e[36m>>>>>> COPYING catalogue service file <<<<<<\e[0m"
-cp catalogue.service /etc/systemd/systsem/catalogue.service
+echo -e "\e[36m>>>>>> copying user service file <<<<<<\e[0m"
+cp user.service /etc/systemd/systsem/user.service
 echo -e "\e[36m>>>>>> daemon-reload <<<<<<\e[0m"
 systemctl daemon-reload
-echo -e "\e[36m>>>>>> enabling the catalogue <<<<<<\e[0m"
-systemctl enable catalogue
-systemctl restart catalogue
+echo -e "\e[36m>>>>>> enabling the user <<<<<<\e[0m"
+systemctl enable user
+systemctl restart user
 echo -e "\e[36m>>>>>> INSTALLING MANGODB <<<<<<\e[0m"
 cp mongo.repo /etc/yum.repo.d/mongo.repo
 yum install mongodb-org-shell-y
 echo -e "\e[36m>>>>>> LOADING SCHEMA <<<<<<\e[0m"
-mongo --host mongodb.tej07.online</app/schema/catalogue.js
+mongo --host mongodb.tej07.online</app/schema/user.js
