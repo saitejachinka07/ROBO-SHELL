eb417408fd88d31ca5ae268196c227b231094b93
saitej <chinka.saiteja@gmail.com>
2023-05-03 19:00:10 +0530

    commit.

--- catalogue.sh
+++ catalogue.sh
@@ -25,4 +25,5 @@ cp /home/centos/ROBO-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
 echo -e "\e[36m>>>>>> LOADING SCHEMA <<<<<<\e[0m"
 mongo --host mongodb.tej07.online </app/schema/catalogue.js
+echo -e "\e[36m>>>>>> Restart catalogue <<<<<<\e[0m"
 systemctl restart catalogue
