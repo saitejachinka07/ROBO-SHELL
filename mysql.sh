2382fedd0f4881af22cc93123ce129e37e2647cd
saitej <chinka.saiteja@gmail.com>
2023-05-04 06:14:50 +0530

    mysql push

---
+++ mysql.sh
@@ -0,0 +1,12 @@
+echo -e "\e[36m>>>>>>>>  disable  mysql module  <<<<<<<<<<<\e[0m"
+dnf module disable mysql -y
+echo -e "\e[36m>>>>>>>>  copying mysql repo file  <<<<<<<<<<<\e[0m"
+cp /home/centos/ROBO-HELL/mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m>>>>>>>> Install mysql  <<<<<<<<<<<\e[0m"
+yum install mysql-community-server -y
+echo -e "\e[36m>>>>>>>> enable mysql  <<<<<<<<<<<\e[0m"
+systemctl enable mysqld
+echo -e "\e[36m>>>>>>>> start mysql  <<<<<<<<<<<\e[0m"
+systemctl start mysqld  
+echo -e "\e[36m>>>>>>>>> Reset Mysql password <<<<<<<<<<\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@
