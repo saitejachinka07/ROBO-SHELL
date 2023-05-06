d3596a0af4bbce33f7580f761f7c0782b9e36d3a
saitej <chinka.saiteja@gmail.com>
2023-05-06 13:53:42 +0530

    echo to mangod

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,12 @@
+echo -e "\e[36m>>>>>>>>>>  copying mongo.repo   <<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>>>  Installing mongodb   <<<<<<<<<<<<\e[0m"
 yum install mongodb-org -y
+echo -e "\e[36m>>>>>>>>>>  Enable mongod   <<<<<<<<<<<<\e[0m"
 systemctl enable mongod 
+echo -e "\e[36m>>>>>>>>>>  Start mongodb  <<<<<<<<<<<<\e[0m"
 systemctl start mongod 
+echo -e "\e[36m>>>>>>>>>>  edit 127.0.0.1 t0 0.0.0.0   <<<<<<<<<<<<\e[0m"
 sed -i -e "s|127.0.0.1|0.0.0.0|" /etc/mongod.conf
+echo -e "\e[36m>>>>>>>>>>  restart mongod   <<<<<<<<<<<<\e[0m"
 systemctl restart mongod
