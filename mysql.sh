cd7678cf49bb4a0d612257adbd7cd88e11128b20
saitej <chinka.saiteja@gmail.com>
2023-05-06 18:06:26 +0530

    roboshop@1 commit

--- mysql.sh
+++ mysql.sh
@@ -9,4 +9,4 @@ systemctl enable mysqld
 echo -e "\e[36m>>>>>>>> start mysql  <<<<<<<<<<<\e[0m"
 systemctl start mysqld  
 echo -e "\e[36m>>>>>>>>> Reset Mysql password <<<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@
+mysql_secure_installation --set-root-pass RoboShop@1
