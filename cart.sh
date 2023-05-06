fe018b0cb3f6f030d8e8bd9137cc22bae368ab87
saitej <chinka.saiteja@gmail.com>
2023-05-06 20:06:00 +0530

    echo {app_user}

--- cart.sh
+++ cart.sh
@@ -1,6 +1,7 @@
 script_path=$(dirname $0)
 source ${script_path}/common.sh
 echo $script_path
+echo ${app_user}
 exit
 echo -e "\e[36m>>>>>>> Installing nodejs <<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
