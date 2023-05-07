47888abd9f5f0ae9f486f8f18ac6cec72eadebf8
saitej <chinka.saiteja@gmail.com>
2023-05-07 13:33:59 +0530

    rabbit passwd

--- payment.sh
+++ payment.sh
@@ -1,7 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-rabbitmq_appuser_password=$1
+rabbit_passwd=$1
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>   Installing python   <<<<<<<<<<<<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>   creatig roboshop user   <<<<<<<<<<<<<<<<<<\e[0m"
@@ -20,7 +20,7 @@ echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>   Installing python dependencies   <<<<<<<<<
 pip3.6 install -r requirements.txt
 
 echo -e "\e[3m>>>>>>>>>>>>>>>>>>>>   soft coding the password  <<<<<<<<<<<<<<<<<<\e[0m"
-sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|"  ${script_path}/payment.service
+sed -i -e "s|rabbit_passwd|${rabbit_passwd}|"  ${script_path}/payment.service
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>   copying payment.service file   <<<<<<<<<<<<<<<<<<\e[0m"
 cp ${script_path}/payment.service /etc/systemd/system/payment.service
 #sed -i -e  "s|rabbitmq_appuser_password|roboshop123|"  /etc/systemd/system/payment.service
