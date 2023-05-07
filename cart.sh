ebc14125f674f34c6a27174b77aba2142ec53b95
saitej <chinka.saiteja@gmail.com>
2023-05-07 18:06:16 +0530

    source func_nodejs(cart)

--- cart.sh
+++ cart.sh
@@ -1,27 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-echo -e "\e[36m>>>>>>> Installing nodejs <<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-echo -e "\e[36m>>>>>> ADDING APPLICATION USER <<<<<<\e[0m"
-useradd ${app_user}
-echo -e "\e[36m>>>>>> CREATING APPLICATION DIRECTORY <<<<<<\e[0m"
-mkdir /app
-echo -e "\e[36m>>>>>> DOWNLOADING APPLICATION CONTENT <<<<<<\e[0m"
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip 
-echo -e "\e[36m>>>>>> REDIRECTING TO /APP DIRECTORY <<<<<<\e[0m"
-cd /app
-echo -e "\e[36m>>>>>> EXTRACTING APPLICATION CONTENT <<<<<<\e[0m"
-unzip /tmp/cart.zip
-cd /app
-echo -e "\e[36m>>>>>> DOWNLOADING NODEJS DEPENDENCIES <<<<<<\e[0m"
-npm install 
-echo -e "\e[36m>>>>>> copying cart service file <<<<<<\e[0m"
-cp ${script_path}/cart.service /etc/systemd/system/cart.service
-echo -e "\e[36m>>>>>> daemon-reload <<<<<<\e[0m"
-systemctl daemon-reload
-echo -e "\e[36m>>>>>> enabling the cart <<<<<<\e[0m"
-systemctl enable cart
-echo -e "\e[36m>>>>>> restart the cart <<<<<<\e[0m"
-systemctl restart cart
+
+component=cart
+
+func_nodejs
--- common.sh
+++ common.sh
@@ -1,1 +1,30 @@
 app_user=roboshop
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+
+func_nodejs() {
+    echo -e "\e[36m>>>>>>> Installing nodejs <<<<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+echo -e "\e[36m>>>>>> ADDING APPLICATION USER <<<<<<\e[0m"
+useradd ${app_user}
+echo -e "\e[36m>>>>>> CREATING APPLICATION DIRECTORY <<<<<<\e[0m"
+mkdir /app
+echo -e "\e[36m>>>>>> DOWNLOADING APPLICATION CONTENT <<<<<<\e[0m"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
+echo -e "\e[36m>>>>>> REDIRECTING TO /APP DIRECTORY <<<<<<\e[0m"
+cd /app
+echo -e "\e[36m>>>>>> EXTRACTING APPLICATION CONTENT <<<<<<\e[0m"
+unzip /tmp/${component}.zip
+cd /app
+echo -e "\e[36m>>>>>> DOWNLOADING NODEJS DEPENDENCIES <<<<<<\e[0m"
+npm install 
+echo -e "\e[36m>>>>>> copying cart service file <<<<<<\e[0m"
+cp ${script_path}/cart.service /etc/systemd/system/cart.service
+echo -e "\e[36m>>>>>> daemon-reload <<<<<<\e[0m"
+systemctl daemon-reload
+echo -e "\e[36m>>>>>> enabling the cart <<<<<<\e[0m"
+systemctl enable ${component}
+echo -e "\e[36m>>>>>> restart the cart <<<<<<\e[0m"
+systemctl restart ${component}
+}
