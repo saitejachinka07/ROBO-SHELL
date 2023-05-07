bbd296de453d1f1fbe692da3371bb565cec8af2e
saitej <chinka.saiteja@gmail.com>
2023-05-07 12:19:57 +0530

    commit no-6

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-mysql_password=$1
+#mysql_password=$1
 echo -e "\e[36m>>>>>>>>  disable  mysql module  <<<<<<<<<<<\e[0m"
 dnf module disable mysql -y
 echo -e "\e[36m>>>>>>>>  copying mysql repo file  <<<<<<<<<<<\e[0m"
