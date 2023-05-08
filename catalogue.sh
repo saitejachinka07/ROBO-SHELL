90753696f978da6c5fb46af6e33c0c97923976e0
saitej <chinka.saiteja@gmail.com>
2023-05-08 10:14:33 +0530

    commit001

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,12 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+
 component=catalogue
+
+schema_setup=mongo
 
 func_nodejs
 
-#echo -e "\e[36m>>>>>> Restart catalogue <<<<<<\e[0m"
-#systemctl restart ${component}
+echo -e "\e[36m>>>>>> Restart catalogue <<<<<<\e[0m"
+systemctl restart ${component}
