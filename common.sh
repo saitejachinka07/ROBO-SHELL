22671b33d1b39d43f18c367e00b783202f1b4a30
saitej <chinka.saiteja@gmail.com>
2023-05-08 12:20:09 +0530

    commit of func_java in common.sh

--- common.sh
+++ common.sh
@@ -53,3 +53,35 @@ systemctl restart ${component}
 
 func_schema_setup
 }
+
+func_java() {
+       func_print_head Installing maven 
+yum install maven -y
+#echo -e "\e[36m>>>>>>> Installing maven <<<<<<<<<\e[0m"
+func_print_head ADDING APPLICATION USER 
+useradd ${app_user}
+func_print_head CREATING APPLICATION DIRECTORY 
+mkdir /app
+func_print_head DOWNLOADING APPLICATION CONTENT 
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
+func_print_head REDIRECTING TO /APP DIRECTORY 
+cd /app
+func_print_head EXTRACTING APPLICATION CONTENT 
+unzip /tmp/shipping.zip
+func_print_head DOWNLOADING MAVEN DEPENDENCIES 
+cd /app 
+mvn clean package 
+mv target/shipping-1.0.jar shipping.jar 
+func_print_head INSTALL MYSQL 
+yum install mysql -y 
+func_print_head LOAD SCHEMA 
+mysql -h mysql.tej07.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql 
+func_print_head DOWNLOADING MAVEN DEPENDENCIES 
+cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
+func_print_head RELOAD DAEMON 
+systemctl daemon-reload
+func_print_head ENABLE SHIPPING 
+systemctl enable shipping
+func_print_head RESTART MYSQL 
+systemctl restart shipping
+}
