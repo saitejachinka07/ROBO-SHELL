9a9460efda8e787096d2db4a8554d34ff8abe3c9
saitej <chinka.saiteja@gmail.com>
2023-05-08 08:36:51 +0530

    if [-z] commit

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,13 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-#mysql_password=$1
+mysql_root_password=$1
+
+if[ -z "${mysql_root_password}" ]; then
+echo "Input mysql_root_password missing"
+exit 
+fi 
+
 echo -e "\e[36m>>>>>>>>  disable  mysql module  <<<<<<<<<<<\e[0m"
 dnf module disable mysql -y
 echo -e "\e[36m>>>>>>>>  copying mysql repo file  <<<<<<<<<<<\e[0m"
@@ -13,4 +19,4 @@ systemctl enable mysqld
 echo -e "\e[36m>>>>>>>> start mysql  <<<<<<<<<<<\e[0m"
 systemctl start mysqld  
 echo -e "\e[36m>>>>>>>>> Reset Mysql password <<<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_root_password}
