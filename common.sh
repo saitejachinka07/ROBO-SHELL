8cb8b00b0e31050e4d1cc6c0b56a1675b18d436a
saitej <chinka.saiteja@gmail.com>
2023-05-08 13:26:15 +0530

    commit 001

--- common.sh
+++ common.sh
@@ -18,8 +18,41 @@ yum install mongodb-org-shell -y
 
 print_head LOADING SCHEMA 
 mongo --host mongodb.tej07.online </app/schema/${component}.js
-
 fi
+if [ "schema_setup" == "mysql" ]; then
+func_print_head INSTALL MYSQL 
+yum install mysql -y 
+func_print_head LOAD SCHEMA 
+mysql -h mysql.tej07.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql 
+fi
+
+}
+
+
+func_app_prereq() {
+func_print_head ADDING APPLICATION USER 
+useradd ${app_user}
+func_print_head CREATING APPLICATION DIRECTORY 
+mkdir /app
+func_print_head DOWNLOADING APPLICATION CONTENT 
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
+func_print_head REDIRECTING TO /APP DIRECTORY 
+cd /app
+func_print_head EXTRACTING APPLICATION CONTENT 
+unzip /tmp/${component}.zip
+
+}
+
+
+func_systemd_setup() {
+func_print_head copying ${component}.service to system        
+cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+func_print_head RELOAD DAEMON 
+systemctl daemon-reload
+func_print_head ENABLE ${component} 
+systemctl enable ${component}
+func_print_head RESTART MYSQL 
+systemctl restart ${component}
 
 }
 
@@ -29,45 +62,25 @@ func_nodejs() {
   func_print_head Installing nodejs 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-func_print_head ADDING APPLICATION USER 
-useradd ${app_user}
-func_print_head CREATING APPLICATION DIRECTORY 
-mkdir /app
- func_print_head DOWNLOADING APPLICATION CONTENT 
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
-func_print_head REDIRECTING TO /APP DIRECTORY 
-cd /app
-func_print_head EXTRACTING APPLICATION CONTENT 
-unzip /tmp/${component}.zip
-cd /app
+
+func_app_prereq 
+
  func_print_head DOWNLOADING NODEJS DEPENDENCIES 
 npm install 
-func_print_head copying ${component} service file 
-cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
-func_print_head daemon-reload 
-systemctl daemon-reload
-func_print_head enabling the ${component} 
-systemctl enable ${component}
-func_print_head restart the ${component} 
-systemctl restart ${component}
 
 func_schema_setup
+
+func_systemd_setup
+
 }
 
 func_java() {
        func_print_head Installing maven 
 yum install maven -y
 #echo -e "\e[36m>>>>>>> Installing maven <<<<<<<<<\e[0m"
-func_print_head ADDING APPLICATION USER 
-useradd ${app_user}
-func_print_head CREATING APPLICATION DIRECTORY 
-mkdir /app
-func_print_head DOWNLOADING APPLICATION CONTENT 
-curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
-func_print_head REDIRECTING TO /APP DIRECTORY 
-cd /app
-func_print_head EXTRACTING APPLICATION CONTENT 
-unzip /tmp/shipping.zip
+
+func_app_prereq
+
 func_print_head DOWNLOADING MAVEN DEPENDENCIES 
 cd /app 
 mvn clean package 
@@ -76,12 +89,9 @@ func_print_head INSTALL MYSQL
 yum install mysql -y 
 func_print_head LOAD SCHEMA 
 mysql -h mysql.tej07.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql 
-func_print_head DOWNLOADING MAVEN DEPENDENCIES 
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-func_print_head RELOAD DAEMON 
-systemctl daemon-reload
-func_print_head ENABLE SHIPPING 
-systemctl enable shipping
-func_print_head RESTART MYSQL 
-systemctl restart shipping
+
+func_schema_setup
+
+func_systemd_setup
+
 }
