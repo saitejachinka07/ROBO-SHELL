44b8674d1c1a6e2c91fd221e2b27106d84f172b2
saitej <chinka.saiteja@gmail.com>
2023-05-08 09:45:04 +0530

    then syntax error in msql

--- mysql.sh
+++ mysql.sh
@@ -3,7 +3,8 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
 
-if[ -z "${mysql_root_password}" ]; then
+if[ -z "${mysql_root_password}" ]
+then
 echo "Input mysql_root_password missing"
 exit 
 fi 
