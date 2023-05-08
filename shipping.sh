b5a5fd3c5ee4f4d761693f365125693fa977d4c6
saitej <chinka.saiteja@gmail.com>
2023-05-08 14:00:35 +0530

    shipping-002

--- shipping.sh
+++ shipping.sh
@@ -2,13 +2,12 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
+
 if [ -z "mysql_root_password" ]; then
-echo Input of mysql_root_password is missing
-exit
+   echo Input of mysql_root_password is missing
+   exit
 fi
 
 component="shipping"
-
 schema_setup=mysql
-
 func_java
