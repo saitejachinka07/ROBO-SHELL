8ea911123523aa77f7f9e225684b2854095de464
saitej <chinka.saiteja@gmail.com>
2023-05-09 18:20:14 +0530

    adding stat_check

--- rerun.sh
+++ rerun.sh
@@ -1,13 +1,29 @@
+stat_check() { 
+if [ $? eq 0 ]; then
+ echo -e "\e[31m||||||SUCESS||||\e[0m]"
+ else 
+  echo -e "\e[31m||||||FAIL||||\e[0m]"
+  exit
+fi
+}
+
+
 echo -e "\e[31m>>>>  cd /app/ <<<<\e[0m]"
 cd /app/
+stat_check
 echo -e "\e[31m>>>>  deleting /app/ content <<<<\e[0m]"
 sudo rm -rf *
+stat_check
 echo -e "\e[31m>>>>  one directory back<<<<\e[0m]"
 cd .. 
+stat_check
 echo -e "\e[31m>>>>  deleting /app/ directory <<<<\e[0m]"
 sudo rmdir /app/
+stat_check
 echo -e "\e[31m>>>>  redirecting to scriptpath <<<<\e[0m]"
-cd ${script_path} 
+cd ${script_path}
+stat_check 
 echo -e "\e[31m>>>>  greping for roboshop uid <<<<\e[0m]"
 
-ps -ef | grep roboshop 
+ps -ef | grep roboshop
+stat_check 
