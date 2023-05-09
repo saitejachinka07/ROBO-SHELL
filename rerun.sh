b2ab68d49d1cb76a9a06f36217bd9cd41cb73662
saitej <chinka.saiteja@gmail.com>
2023-05-09 18:14:40 +0530

    adding echo command to rerun

--- rerun.sh
+++ rerun.sh
@@ -1,8 +1,13 @@
+echo -e "\e[31m>>>>  cd /app/ <<<<\e[0m]"
 cd /app/
+echo -e "\e[31m>>>>  deleting /app/ content <<<<\e[0m]"
 sudo rm -rf *
+echo -e "\e[31m>>>>  one directory back<<<<\e[0m]"
 cd .. 
+echo -e "\e[31m>>>>  deleting /app/ directory <<<<\e[0m]"
 sudo rmdir /app/
-
+echo -e "\e[31m>>>>  redirecting to scriptpath <<<<\e[0m]"
 cd ${script_path} 
+echo -e "\e[31m>>>>  greping for roboshop uid <<<<\e[0m]"
 
 ps -ef | grep roboshop 
